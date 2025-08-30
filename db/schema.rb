0f6f901987e64f6e34246e8d282d3ef4b86ca4bd
AndPewka <sayan-andrey@mail.ru>
2025-08-30 09:54:29 +0700

    add category unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_30_023017) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_30_025029) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_30_023017) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
